d36127a2add70a315c9909c60de38c9f9c42a6bd
kennylam91 <phamvanlam1991@gmail.com>
2019-08-07 11:24:56 +0700

    add validator and error to sign-up component

--- src/app/component/sign-up/sign-up.component.ts
+++ src/app/component/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IUser} from '../../model/user';
 import {FormControl, FormGroup} from '@angular/forms';
 import {UserService} from '../../service/user.service';
 import {Router} from '@angular/router';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,14 +13,14 @@ import {Router} from '@angular/router';
 export class SignUpComponent implements OnInit {
   newUser: IUser;
   userRegisterForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
+    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])
   });
 
   onSubmit() {
     this.newUser = {
-      username: this.userRegisterForm.controls.username.value,
-      password: this.userRegisterForm.controls.password.value,
+      username: this.userRegisterForm.value.username,
+      password: this.userRegisterForm.value.password,
       status: 'offline',
     };
     this.userService.addtoUserList(this.newUser);
